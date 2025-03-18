7534314423b46e66b971e416f2709cc5baaf6a9c
Rialahmed0 <rashedahommod0@gmail.com>
2025-03-19 03:15:49 +0600

    menu section

---
+++ script.js
@@ -0,0 +1,24 @@
+let filterButton = document.querySelectorAll(".filter-button  button");
+let filterableCard = document.querySelectorAll(".filterable-card .card")
+console.log(filterButton);
+console.log(filterableCard);
+
+let filterCard = (e) => {
+    // add here active functionality 
+    document.querySelector(".active").classList.remove('active');
+    e.target.classList.add('active')
+
+    filterableCard.forEach(card => {
+        card.classList.add('hidden');
+
+        if (card.dataset.name === e.target.dataset.name || card.dataset.name === 'all') {
+            card.classList.remove('hidden');
+
+        }
+    })
+
+};
+
+
+// add click event in all buttons 
+filterButton.forEach(button => button.addEventListener('click', filterCard))
